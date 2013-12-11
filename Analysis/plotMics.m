c64b03ed9c537468a69f105a018082d1295fcc17
Eric Whitmire <ewhitmire91@gmail.com>
2013-12-11 16:02:47 -0500

    Changed matlab to asynchronous reads

--- Analysis/plotMics.m
+++ Analysis/plotMics.m
@@ -1,98 +1,85 @@
-try
-    fclose(port)
-catch
-end
-close all;
-
-port = serial('COM7','BaudRate',115200, 'FlowControl', 'hardware');
-fopen(port);
-
-% signal = [255, 255, 255, 255, 255, 255];
-% buffer = zeros(1, length(signal));
-% while (1)
-%     data = fread(port, 1, 'uint8')';
-%     buffer = [data(1), buffer(1:end-1)]
-%     if signal==buffer
-%         break;
-%     end
-% end
-figure; hold on;
-size = 93;
-channels = 2;
-data = zeros(channels,size);
-ah = zeros(1, channels);
-for channel = 1:channels
-    ah(channel) = subplot(channels,1,channel);
-end
-%figure; hold on;
-
-plotSamples = 1000;
-plotBuffer = zeros(channels,plotSamples);
-index = 1;
-drawCounter = 0;
-signal = [255, 255, 255, 255, 255, 255];
-sampleSize = 1;
-recording = [];
-lastPlay = 1;
-playCount = 1;
-
-fwrite(port, '*')
-
-len = 12;
-while (1)
-    
-    buffer = zeros(1, length(signal));
-    %while (1)
-    %    data = fread(port, 1, 'uint8')';
-    %    buffer = [data(1), buffer(1:end-1)];
-    %    if signal==buffer
-    %        break;
-    %    end
-    %end
-    
-    %len = fread(port, 1, 'uint16');
-    %if (len ~= 90)
-    %    x = 0
-    %end
-%while(len > 1000 || len <=0 )
-%    len = fread(port, 1, 'uint16')
-%end
-    newData = fread(port, len/sampleSize, ['int',num2str(8*sampleSize)])';
-    if (min(newData) == -1)
-        x = 0
-        len
-        newData
+function plotMics()
+    global port plotBuffer readSamples channels drawCounter sampleSize
+    try
+        fclose(port)
+    catch
     end
-    newData = reshape(newData, channels, length(newData)/channels);
-    %recording = [recording, newData];
-    if sum(newData(1,:) > 100)
-        t = 3;
+    close all;
+
+    sampleSize = 1;
+    plotSamples = 5000;
+    readSamples = 40;
+    channels = 2;
+    scale = 3.3;
+
+    port = serial('COM7','BaudRate',115200, 'FlowControl', 'hardware');
+    port.BytesAvailableFcnCount = readSamples;
+    port.BytesAvailableFcnMode = 'byte';
+    port.BytesAvailableFcn = @serial_callback;
+    fopen(port);
+
+    % signal = [255, 255, 255, 255, 255, 255];
+    % buffer = zeros(1, length(signal));
+    % while (1)
+    %     data = fread(port, 1, 'uint8')';
+    %     buffer = [data(1), buffer(1:end-1)]
+    %     if signal==buffer
+    %         break;
+    %     end
+    % end
+    figure; hold on;
+    size = 93;
+    data = zeros(channels,size);
+    ah = zeros(1, channels);
+    for channel = 1:channels
+        ah(channel) = subplot(channels,1,channel);
     end
+    %figure; hold on;
 
-    plotBuffer = [plotBuffer(:,len/channels/sampleSize+1:end), newData];
+    plotBuffer = zeros(channels,plotSamples);
+    index = 1;
+    drawCounter = 0;
+    recording = [];
+    lastPlay = 1;
+    playCount = 1;
+
+    len = 12
     
+    fwrite(port, '*')
 
-    if drawCounter == plotSamples/20
-        for channel = 1:channels
-            axes(ah(channel));
-            cla;
-            plot(plotBuffer(channel,:));
-            ylim([0, 2^(8*sampleSize-1)])
+    while (1)
+        pause(0.005);
+        %if drawCounter >= plotSamples/20
+            for channel = 1:channels
+                axes(ah(channel));
+                cla;
+                plot(plotBuffer(channel,:).*scale ./ 2^(8*sampleSize-1));
+                ylim([0, scale])
+            end
+            drawnow;
+            refresh;
+            drawCounter = 0;
+        %end
+        %playCount = playCount + len/channels/sampleSize;
+        if playCount-lastPlay > 25000
+            sample = recording(1,lastPlay:end);
+            isNonZero = sample > 2;
+            sample = sample .* isNonZero;
+            %p = audioplayer(sample,5000);
+            %p.play();
+            lastPlay = length(recording)+1;
         end
-        drawnow;
-        refresh;
-        drawCounter = 0;
     end
-    drawCounter = drawCounter + 1;
-    %playCount = playCount + len/channels/sampleSize;
-    if playCount-lastPlay > 25000
-        sample = recording(1,lastPlay:end);
-        isNonZero = sample > 2;
-        sample = sample .* isNonZero;
-        %p = audioplayer(sample,5000);
-        %p.play();
-        lastPlay = length(recording)+1;
-    end
-end
 
-fclose(port);
+    fclose(port);
+end
+function serial_callback(obj,event)
+    global port plotBuffer channels drawCounter readSamples sampleSize
+    newData = fread(port, readSamples/sampleSize, ['int',num2str(8*sampleSize)])';
+    newData = reshape(newData, channels, length(newData)/channels);
+    
+    
+    drawCounter = drawCounter + readSamples;
+    plotBuffer = [plotBuffer(:,readSamples/channels/sampleSize+1:end), newData];
+        
+end
