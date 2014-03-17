86aee8e630664dabfe240fbccc441ed4c02a756f
Eric Whitmire <ewhitmire91@gmail.com>
2014-03-17 03:17:42 -0400

    moving spi

--- RoachZStack/Source/spi.c
+++ RoachZStack/Source/spi.c
@@ -1,16 +1,16 @@
 #include "spi.h"
 #include "OnBoard.h"
 
-#define MISO_PIN 4
-#define MOSI_PIN 5
-#define SCK_PIN 3
+#define MISO_PIN 7
+#define MOSI_PIN 6
+#define SCK_PIN 5
 #define CS_PIN 0
 
 void SPI_Init()
 {
   
   // Setup SPI
-  PERCFG |= 0x1 << 0;     // UART 0 in Alt 2
+  PERCFG |= 0x1 << 1;     // UART 1 in Alt 2
   P1SEL |= 0x1 << SCK_PIN;    // Set pins as output
   P1SEL |= 0x1 << MOSI_PIN;
   P1SEL |= 0x1 << MISO_PIN;
@@ -21,15 +21,15 @@ void SPI_Init()
   // Set baud rate to max (system clock frequency / 8) 
   // Assuming a 32 MHz crystal (CC1110Fx/CC2510Fx), 
   // max baud rate = 32 MHz / 8 = 4 MHz. 
-  U0BAUD = 0x00; // BAUD_M = 0 
-  U0GCR |= 0x11; // BAUD_E = 17
+  U1BAUD = 0x00; // BAUD_M = 0 
+  U1GCR |= 0x11; // BAUD_E = 17
   
   // SPI Master Mode
-  U0CSR &= ~0xA0;
+  U1CSR &= ~0xA0;
   
   // Configure phase, polarity, and bit order 
-  U0GCR &= ~0xC0; // CPOL = 0; CPHA = 0
-  U0GCR |= 0x20; // ORDER = 1 
+  U1GCR &= ~0xC0; // CPOL = 0; CPHA = 0
+  U1GCR |= 0x20; // ORDER = 1 
   
   
   P1 |= (0x1<<CS_PIN);
@@ -40,9 +40,9 @@ void SPI_Write(uint8 len, uint8* buffer)
   P1 &= ~(0x1<<CS_PIN);
   for (int i = 0; i <= len; i++) 
   { 
-   U0DBUF = buffer[i]; 
-   while (!(U0CSR & 0x02)); 
-   U0CSR &= ~0x02;
+   U1DBUF = buffer[i]; 
+   while (!(U1CSR & 0x02)); 
+   U1CSR &= ~0x02;
   } 
   P1 |= (0x1<<CS_PIN);
 }
