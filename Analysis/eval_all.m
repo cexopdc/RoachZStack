14e81d4732804ff10e329bde0320642a9a2767fd
Eric Whitmire <ewhitmire91@gmail.com>
2014-07-17 12:07:01 -0400

    plot error

--- Analysis/eval_all.m
+++ Analysis/eval_all.m
@@ -31,5 +31,26 @@ function err = eval_all(fits, data, angles, loudness)
     ylabel('Distance (in)');
     zlabel('Amplitude');
     
+    % plot first entry
+    pred = pred_angles(:,1)';
+    for i = 1:length(pred)
+       if i < length(pred) / 3
+           if pred(i) > 180
+               pred(i) = pred(i) - 360
+           end
+       elseif i > length(pred) * 2 / 3
+           if pred(i) < 180
+               pred(i) = pred(i) + 360
+           end
+       end
+    end
+    
+    figure; hold on;
+    plot(angles, pred);
+    plot(angles, angles);
+    legend('Estimation', 'Actual');
+    xlabel('Actual Angle (deg)');
+    ylabel('Estimated Angle (deg)');
+    
     err = mean(mean(errors));
 end
