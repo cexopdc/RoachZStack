91aaa9d26c311ff9cfbd74ea64f9186264de8177
hxiong <hxiong@ncsu.edu>
2015-06-23 00:41:21 -0400

    Revert 6c7ad47..f327eb5
    This rolls back to commit 6c7ad472f44be7bb15ee695f986bb5529b555fde.

--- Analysis/RSSI/kick_sys/Large scale/DV_distance/area_space_changing_main.m
+++ Analysis/RSSI/kick_sys/Large scale/DV_distance/area_space_changing_main.m
@@ -2,7 +2,7 @@ close all; clear all; clc;
 rng default;
 
 start_point = 20;
-end_point = 200; 
+end_point = 100; 
 num_trials = 30;  %%%%%%%%%%%%
 error_matrix=[];
 aggregate_error_matrix=[];
@@ -14,20 +14,21 @@ for i=start_point:20:end_point % number of nodes
     aggregate_coverage = 0;
     fprintf('i=%f\n',i);
     for j=1:num_trials % number of trials
-        [average_loc_error_array,coverage,avg_connectivity] = main_CRLB_ONLY(100,i,20,0.2,0.2);
+        [average_loc_error_array,coverage,avg_connectivity] = main(100,i,20,0.2,0.2);
         aggregate_error=[aggregate_error;average_loc_error_array];
         aggregate_connectivity_counter = aggregate_connectivity_counter + avg_connectivity;
         aggregate_coverage = aggregate_coverage + coverage;
     end
     aggregate_error_matrix = [aggregate_error_matrix;aggregate_error];
-    error_matrix = [error_matrix;mean(nonzeros(aggregate_error(:,1)))]; 
+    error_matrix = [error_matrix;mean(nonzeros(aggregate_error(:,1))) mean(nonzeros(aggregate_error(:,2))) mean(nonzeros(aggregate_error(:,3))) ...
+        mean(nonzeros(aggregate_error(:,4))) mean(nonzeros(aggregate_error(:,5))) mean(nonzeros(aggregate_error(:,6))) mean(nonzeros(aggregate_error(:,7)))]; 
     connectivity_array = [connectivity_array aggregate_connectivity_counter/num_trials];
     coverage_matrix = [coverage_matrix aggregate_coverage/num_trials];
 end
 
 error_matrix = error_matrix';
 
-save area_space_20_to_200_30trials_CRLB_ONLY.mat; % Remember to change the file name
+save area_space_20_to_100_30trials.mat; % Remember to change the file name
 
 figure;
 x = start_point:20:end_point;
--- Analysis/RSSI/kick_sys/Large scale/DV_distance/trans_range_changing_main.m
+++ Analysis/RSSI/kick_sys/Large scale/DV_distance/trans_range_changing_main.m
@@ -14,20 +14,21 @@ for i=start_point:10:end_point % number of nodes
     aggregate_coverage = 0;
     fprintf('i=%f\n',i);
     for j=1:num_trials % number of trials
-        [average_loc_error_array,coverage,avg_connectivity] = main_CRLB_ONLY(100,100,i,0.2,0.2);  %%%%%%%%%%%
+        [average_loc_error_array,coverage,avg_connectivity] = main(100,100,i,0.2,0.2);
         aggregate_error=[aggregate_error;average_loc_error_array];
         aggregate_connectivity_counter = aggregate_connectivity_counter + avg_connectivity;
         aggregate_coverage = aggregate_coverage + coverage;
     end
     aggregate_error_matrix = [aggregate_error_matrix;aggregate_error];
-    error_matrix = [error_matrix;mean(nonzeros(aggregate_error(:,1)))]; 
+    error_matrix = [error_matrix;mean(nonzeros(aggregate_error(:,1))) mean(nonzeros(aggregate_error(:,2))) mean(nonzeros(aggregate_error(:,3))) ...
+        mean(nonzeros(aggregate_error(:,4))) mean(nonzeros(aggregate_error(:,5))) mean(nonzeros(aggregate_error(:,6))) mean(nonzeros(aggregate_error(:,7)))]; 
     connectivity_array = [connectivity_array aggregate_connectivity_counter/num_trials];
     coverage_matrix = [coverage_matrix aggregate_coverage/num_trials];
 end
 
 error_matrix = error_matrix';
 
-save trans_range_10_to_50_50trials_CRLB_ONLY.mat; % Remember to change the file name
+save trans_range_10_to_50_50trials.mat; % Remember to change the file name
 
 figure;
 x = start_point:10:end_point;
